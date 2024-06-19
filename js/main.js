707f391611ac636cc835c73f5593cdb1f941ada4
Maksym Ryaboshapka <125752351+Maksym-Ryaboshapka@users.noreply.github.com>
2024-06-19 13:12:27 +0300

    fix while loops

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // Task 1
 
-let i = 0;
+let i = 1;
 
 while (i < 10) {
-	i++;
+  console.log(i);
 
-	console.log(i);
+  i++;
 }
 
 // Task 2
@@ -13,26 +13,26 @@ while (i < 10) {
 for (let j = 2; j <= 20; j++) {
   if (j % 2 !== 0) {
     continue;
-	}
+  }
 
-	console.log(j);
+  console.log(j);
 }
 
 // Task 3
 
 for (let k = 1; k <= 10; k++) {
-	console.log(`${k} * 7 = ${k * 7}`);
+  console.log(`${k} * 7 = ${k * 7}`);
 }
 
 // Task 4
 
 const arr1 = [1, 2, 3, 4, 5];
-let l = -1;
+let l = 0;
 
 while (l < 4) {
-  l++;
-
   console.log(arr1[l]);
+
+  l++;
 }
 
 // Task 5
@@ -54,9 +54,9 @@ const number = prompt("Введіть число");
 for (let n = 1; n < number; n++) {
   if (n >= number) {
     break;
-	}
+  }
 
-	alert(n);
+  alert(n);
 }
 
 // Task 7
@@ -64,11 +64,11 @@ for (let n = 1; n < number; n++) {
 let o = 0;
 
 while (o < 20) {
-  o++;
-
   if (o % 3 === 0) {
     continue;
   }
 
   console.log(o);
+
+  o++;
 }
